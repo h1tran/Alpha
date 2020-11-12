0be063438198ccd3bcdb8c851bec2c57d90e7576
Henry Tran <h98.tran@gmail.com>
2020-11-12 17:38:12 -0500

    Day 2.1 - Templates + Flask Code Blocks

--- app.py
+++ app.py
@@ -1,17 +1,32 @@
-from flask import Flask, escape, request
+from flask import Flask, render_template, url_for
 
 # Double underscore name is a special variable in Python that is the name of the module.
 app = Flask(__name__)
+
+posts = [
+    {
+        'author': 'Henry Tran',
+        'title': 'Hello World!',
+        'content': 'First post!',
+        'date_posted': 'November 11, 2020'
+    },
+    {
+        'author': 'Jane Doe',
+        'title': 'Welcome Henry!',
+        'content': 'Welcome to Alpha!',
+        'date_posted': 'November 12, 2020'
+    }
+]
 
 # Route decorators | Seems similar to linking in React (?)
 @app.route('/')
 @app.route('/home')
 def home():
-    return '<h1>Hello World!</h1>'
+    return render_template('home.html', posts=posts)
 
 @app.route('/about')
 def about():
-    return '<h1>About Page</h1>'
+    return render_template('about.html', title='About')
 
 if __name__ == '__main__':
     app.run(debug=True)
